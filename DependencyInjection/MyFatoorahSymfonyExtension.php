40ef4edca77f5dc3ab7be7f386e26443c3df7ca0
rs <rsaeed@myfatoorah.com>
2022-07-07 16:20:26 +0200

    test

--- DependencyInjection/MyFatoorahSymfonyExtension.php
+++ DependencyInjection/MyFatoorahSymfonyExtension.php
@@ -2,17 +2,15 @@
 
 namespace MyFatoorah\SymfonyBundle\DependencyInjection;
 
-
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
-
 class MyFatoorahSymfonyExtension extends Extension {
 
     public function load(array $configs, ContainerBuilder $container) {
-        
+
         error_log('11111111111111111111111111111111111111111111111111111111111111111111111111111111111');
         $loader = new YamlFileLoader(
                 $container,
--- src/MyFatoorahSymfonyBundle.php
+++ src/MyFatoorahSymfonyBundle.php
@@ -3,27 +3,44 @@
 namespace MyFatoorah\SymfonyBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use MyFatoorah\SymfonyBundle\DependencyInjection\MyFatoorahSymfonyExtension;
+use MyFatoorah\SymfonyBundle\DependencyInjection\UnconventionalExtensionClass;
 
 class MyFatoorahSymfonyBundle extends Bundle {
 
-    public function getPath(): string {
-        error_log('22222222222222222222222222222222222222222222222222222222222222222222222222222222222');
-        return \dirname(__DIR__);
-    }
+//    public function getPath(): string {
+//        error_log('22222222222222222222222222222222222222222222222222222222222222222222222222222222222');
+//        return \dirname(__DIR__);
+//    }
+//
+////    public function getContainerExtension() {
+////        error_log('33333333333333333333333333333333333333333333333333333333333333333333333333333333333');
+////
+////        return new UnconventionalExtensionClass();
+////    }
+//    
+//    public function getAlias(){
+//        error_log('44444444444444444444444444444444444444444444444444444444444444444444444444444444444');
+//        $extension = $this->createContainerExtension();
+////        error_log('5555555' . $extension->getAlias() . '5555555');
+//        error_log('44444444444444444444444444444444444444444444444444444444444444444444444444444444444');
+//        return 'myfatoorah_symfony';
+//    }
 
-    public function getContainerExtension() {
-        error_log('33333333333333333333333333333333333333333333333333333333333333333333333333333333333');
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void {
+        // load an XML, PHP or Yaml file
+        $container->import('../config/services.yaml');
 
-        return new MyFatoorahSymfonyExtension();
-    }
-    
-    public function getAlias(){
-        error_log('44444444444444444444444444444444444444444444444444444444444444444444444444444444444');
-        $extension = $this->createContainerExtension();
-//        error_log('5555555' . $extension->getAlias() . '5555555');
-        error_log('44444444444444444444444444444444444444444444444444444444444444444444444444444444444');
-        return 'myfatoorah_symfony';
+        // you can also add or replace parameters and services
+        $container->parameters()
+                ->set('myfatoorah_symfony.phrase', $config['phrase'])
+        ;
+
+        if ($config['scream']) {
+            $container->services()
+                    ->get('myfatoorah_symfony.printer')
+                    ->class(ScreamingPrinter::class)
+            ;
+        }
     }
 
 }
